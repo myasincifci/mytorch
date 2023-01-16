1dd98a8103ca9f10c9a9febf4205f9ad7eb25bd6
Mehmet Yasin Cifci <cifci@campus.tu-berlin.de>
2023-01-16 16:32:51 +0100

    working on backprop

--- tensor.py
+++ tensor.py
@@ -1,31 +1,63 @@
 import numpy as np
+from copy import deepcopy
 
 class Tensor(np.ndarray):
     def __new__(cls, input):
-        obj = np.array(input).view(cls)
+        obj = np.array(input).astype(np.float64).view(cls)
         return obj
 
+    def __init__(self, _) -> None:
+        self.grad = None
+        self._prev = None
+        self._op = None
+        self.label = None
+
+        self._backward = lambda: None
+    
+    def set_attributes(self, _prev, _op):
+        self._prev = _prev # supposed to be set but Tensor is not hashable yet
+        self._op = _op
+
+        return self
+
     def __add__(self, other):
-        return super().__add__(other)
-
-    def __sub__(self, other):
-        return super().__sub__(other)
-
-    def __mul__(self, other):
-        return super().__mul__(other)
-
-    def __div__(self, other):
-        return super().__mul__(other)
+        out = super().__add__(other).set_attributes((self, other), '+')
+        
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
+        
+        return out 
 
     def __matmul__(self, other):
-        return super().__matmul__(other)
+        out = super().__matmul__(other).set_attributes((self, other), '@')
+        return out
+
+    def relu(self):
+        out = (self * (self > 0).astype(np.float64)).set_attributes((self,), 'relu')
+
+        def _backward():
+            g = self * (self > 0).astype(np.float64)
+            self.grad = g * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        pass
 
 def main():
-    t = Tensor([
+    W = Tensor([
         [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+        [4, 5, 6]
     ])
+    x = Tensor([1, 1, 1])
+    b = Tensor([1, 1])
+
+    y = W @ x
+    y = y + b
+    y = y.relu()
 
     a=1
 
